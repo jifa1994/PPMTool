d9049ea1186c7858cdc48664fa6864a43a9080c2
Fa Ji <faji199417@gmail.com>
2019-09-12 19:02:18 -0400

    fix

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -1,20 +1,21 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { React } from "react";
 
 const initalState = {};
 const middleware = [thunk];
 
 let store;
-
-if (window.navigator.userAgent.includes("Chrome")) {
+const ReactReduxDecTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+if (window.navigator.userAgent.includes("Chrome") && ReactReduxDecTools) {
   store = createStore(
     rootReducer,
     initalState,
     compose(
       applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      ReactReduxDecTools
     )
   );
 } else {
